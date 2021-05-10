059d270d305870f67760e4cb976cc64dafa7e740
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:11:24 -0500

    Add files via upload

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+variable1  =  int ( input ( "Ingrese un número entero:" ))
+variable2  =  int ( input ( "Ingrese otro número:" ))
+print ( "Los valores iniciales son: a =" , variable1 , "yb =" , variable2 )
+cambio1  =  variable2
+cambio2  =  variable1
+print ( "Los valores intercambiados son a =" , cambio1 , "yb =" , cambio2 )
